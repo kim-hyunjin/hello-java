1f54b35ceda5dde986fd5ae6189e8ec0f7e29856
kim-hyunjin <hyunjin1612@gmail.com>
2020-02-14 18:05:54 +0900

    dbms사용

--- bitcamp-project-client/src/main/java/com/eomcs/lms/handler/LessonListCommand.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/handler/LessonListCommand.java
@@ -1,8 +1,10 @@
 package com.eomcs.lms.handler;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import com.eomcs.lms.dao.LessonDao;
-import com.eomcs.lms.domain.Lesson;
 
 public class LessonListCommand implements Command {
 
@@ -15,11 +17,37 @@ public class LessonListCommand implements Command {
   @Override
   public void execute() {
     try {
-      List<Lesson> lessons = lessonDao.findAll();
-      for (Lesson l : lessons) {
-        System.out.printf("%d, %s, %s ~ %s, %d\n", l.getNo(), l.getTitle(), l.getStartDate(),
-            l.getEndDate(), l.getTotalHours());
+      //List<Lesson> lessons = lessonDao.findAll();
+      
+   // JDBC Driver(MariaDB 프록시)를 로딩한다.
+      Class.forName("org.mariadb.jdbc.Driver");
+
+      // JDBC Driver를 이용하여 MariaDB 에 접속한다.
+      Connection con = DriverManager.getConnection( //
+          "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
+
+      // MariaDB에 명령을 전달할 객체 준비
+      Statement stmt = con.createStatement();
+
+      // MariaDB의 lms_lesson 테이블에 있는 데이터를 가져올 도구를 준비
+      ResultSet rs = stmt.executeQuery( //
+          "select lesson_id, titl, conts, sdt, edt, tot_hr, day_hr from lms_lesson");
+
+      // ResultSet 도구를 사용하여 데이터를 하나씩 가져온다.
+      while (rs.next()) { // 데이터를 한 개 가져왔으면 true를 리턴한다.
+        System.out.printf("%d, %s, %s, %s~%s, %d, %d\n", //
+            rs.getInt("lesson_id"), //
+            rs.getString("titl"), //
+            rs.getString("conts"),
+            rs.getDate("sdt"),
+            rs.getDate("edt"),
+            rs.getInt("tot_hr"), //
+            rs.getInt("day_hr")
+        );
       }
+      rs.close();
+      stmt.close();
+      con.close();
     } catch (Exception e) {
       System.out.println("목록조회 실패!");
     }
--- bitcamp-project-client/src/main/java/com/eomcs/lms/handler/MemberListCommand.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/handler/MemberListCommand.java
@@ -1,8 +1,10 @@
 package com.eomcs.lms.handler;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import com.eomcs.lms.dao.MemberDao;
-import com.eomcs.lms.domain.Member;
 
 public class MemberListCommand implements Command {
 
@@ -15,11 +17,36 @@ public class MemberListCommand implements Command {
   @Override
   public void execute() {
     try {
-      List<Member> members = memberDao.findAll();
-      for (Member m : members) {
-        System.out.printf("%d, %s, %s, %s, %s\n", m.getNo(), m.getName(), m.getEmail(), m.getTel(),
-            m.getRegisteredDate());
+      //List<Member> members = memberDao.findAll();
+   // JDBC Driver(MariaDB 프록시)를 로딩한다.
+      Class.forName("org.mariadb.jdbc.Driver");
+
+      // JDBC Driver를 이용하여 MariaDB 에 접속한다.
+      Connection con = DriverManager.getConnection( //
+          "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
+
+      // MariaDB에 명령을 전달할 객체 준비
+      Statement stmt = con.createStatement();
+
+      // MariaDB의 lms_member 테이블에 있는 데이터를 가져올 도구를 준비
+      ResultSet rs = stmt.executeQuery( //
+          "select member_id, name, email, cdt, tel, photo from lms_member");
+
+      // ResultSet 도구를 사용하여 데이터를 하나씩 가져온다.
+      while (rs.next()) { // 데이터를 한 개 가져왔으면 true를 리턴한다.
+        System.out.printf("%d, %s, %s, %s, %s, %s\n", //
+            rs.getInt("member_id"), //
+            rs.getString("name"), //
+            rs.getString("email"),
+            rs.getDate("cdt"),
+            rs.getString("tel"),
+            rs.getString("photo")
+        );
       }
+      rs.close();
+      stmt.close();
+      con.close();
+      
     }catch(Exception e) {
       System.out.println("목록조회 실패!");
 
