ab726a252fdd01d77344fc4e4c2f65b11ab51a86
kim-hyunjin <hyunjin1612@gmail.com>
2020-02-21 18:15:18 +0900

    Application Server 구조로 변경

---
+++ bitcamp-project-server/version/v37_1/src/test/java/com/eomcs/lms/Test.java
@@ -0,0 +1,10 @@
+package com.eomcs.lms;
+
+public class Test {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ bitcamp-project-server/version/v37_2/src/main/java/com/eomcs/lms/ServerApp.java
@@ -0,0 +1,154 @@
+// LMS 서버
+package com.eomcs.lms;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import com.eomcs.lms.context.ApplicationContextListener;
+import com.eomcs.lms.dao.BoardDao;
+import com.eomcs.lms.dao.LessonDao;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.servlet.BoardListServlet;
+import com.eomcs.lms.servlet.LessonListServlet;
+import com.eomcs.lms.servlet.MemberListServlet;
+import com.eomcs.lms.servlet.Servlet;
+
+public class ServerApp {
+
+  // 옵저버 관련 코드
+  Set<ApplicationContextListener> listeners = new HashSet<>();
+  Map<String, Object> context = new HashMap<>();
+
+  // 커맨드(예: Servlet 구현체) 디자인 패턴과 관련된 코드
+  Map<String, Servlet> servletMap = new HashMap<>();
+
+  // 스레드 풀
+  ExecutorService executorService = Executors.newCachedThreadPool();
+
+  public void addApplicationContextListener(ApplicationContextListener listener) {
+    listeners.add(listener);
+  }
+
+  public void removeApplicationContextListener(ApplicationContextListener listener) {
+    listeners.remove(listener);
+  }
+
+  private void notifyApplicationInitialized() {
+    for (ApplicationContextListener listener : listeners) {
+      listener.contextInitialized(context);
+    }
+  }
+
+  private void notifyApplicationDestroyed() {
+    for (ApplicationContextListener listener : listeners) {
+      listener.contextDestroyed(context);
+    }
+  }
+  // 옵저버 관련코드 끝!
+
+  public void service() {
+
+    notifyApplicationInitialized();
+
+    // DataLoaderListener가 준비한 DAO 객체를 꺼내 변수에 저장한다.
+    BoardDao boardDao = (BoardDao) context.get("boardDao");
+    LessonDao lessonDao = (LessonDao) context.get("lessonDao");
+    MemberDao memberDao = (MemberDao) context.get("memberDao");
+
+    // 커맨드 객체 역할을 수행하는 서블릿 객체를 맵에 보관한다.
+    servletMap.put("/board/list", new BoardListServlet(boardDao));
+    // servletMap.put("/board/add", new BoardAddServlet(boardDao));
+    // servletMap.put("/board/detail", new BoardDetailServlet(boardDao));
+    // servletMap.put("/board/update", new BoardUpdateServlet(boardDao));
+    // servletMap.put("/board/delete", new BoardDeleteServlet(boardDao));
+    //
+    servletMap.put("/lesson/list", new LessonListServlet(lessonDao));
+    // servletMap.put("/lesson/add", new LessonAddServlet(lessonDao));
+    // servletMap.put("/lesson/detail", new LessonDetailServlet(lessonDao));
+    // servletMap.put("/lesson/update", new LessonUpdateServlet(lessonDao));
+    // servletMap.put("/lesson/delete", new LessonDeleteServlet(lessonDao));
+    //
+    servletMap.put("/member/list", new MemberListServlet(memberDao));
+    // servletMap.put("/member/add", new MemberAddServlet(memberDao));
+    // servletMap.put("/member/detail", new MemberDetailServlet(memberDao));
+    // servletMap.put("/member/update", new MemberUpdateServlet(memberDao));
+    // servletMap.put("/member/delete", new MemberDeleteServlet(memberDao));
+
+    try (ServerSocket serverSocket = new ServerSocket(9999)) {
+
+      System.out.println("클라이언트 연결 대기중...");
+
+      while (true) {
+        Socket socket = serverSocket.accept();
+        System.out.println("클라이언트와 연결되었음!");
+
+        executorService.submit(() -> {
+          processRequest(socket);
+        });
+      }
+
+    } catch (Exception e) {
+      System.out.println("서버 준비 중 오류 발생!");
+      e.printStackTrace();
+    }
+
+    notifyApplicationDestroyed();
+    executorService.shutdown();
+  } // service()
+
+
+  int processRequest(Socket clientSocket) {
+
+    try (Socket socket = clientSocket;
+        Scanner in = new Scanner(socket.getInputStream());
+        PrintStream out = new PrintStream(socket.getOutputStream())) {
+
+      // 클라이언트가 보낸 명령을 읽는다.
+      String request = in.nextLine();
+      // System.out.println(request);
+
+      // if (request.equalsIgnoreCase("/server/stop")) {
+      // System.out.println("서버를 종료합니다.");
+      // return 9;
+      // }
+      Servlet servlet = servletMap.get(request);
+      if (servlet != null) {
+        try {
+          servlet.service(in, out);
+        } catch (Exception e) {
+          out.println("요청 처리 중 오류 발생!");
+          e.printStackTrace();
+        }
+      } else { // 없다면? 간단한 아내 메시지를 응답한다.
+        notFound(out);
+      }
+      out.println("!end!");
+      out.flush();
+      System.out.println("클라이언트에게 응답하였음!");
+      return 0;
+    } catch (Exception e) {
+      System.out.println("예외 발생:");
+      e.printStackTrace();
+      return -1;
+    }
+  }// processRequest
+
+  private void notFound(PrintStream out) throws IOException {
+    out.println("요청한 명령을 처리할 수 없습니다.");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("서버 수업 관리 시스템입니다.");
+    ServerApp app = new ServerApp();
+    app.addApplicationContextListener(new DataLoaderListener());
+    app.service();
+  }
+}
