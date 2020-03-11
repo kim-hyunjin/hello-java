eb76d63b55efb6ce8b6460ce27fee6e493642d06
kim-hyunjin <hyunjin1612@gmail.com>
2020-03-11 10:24:07 +0900

    server 43_1버전 백업

---
+++ bitcamp-project-server/version/v43_1/src/main/java/com/eomcs/lms/DataLoaderListener.java
@@ -0,0 +1,66 @@
+package com.eomcs.lms;
+
+import java.io.InputStream;
+import java.util.Map;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.eomcs.lms.context.ApplicationContextListener;
+import com.eomcs.lms.dao.mariadb.BoardDaoImpl;
+import com.eomcs.lms.dao.mariadb.LessonDaoImpl;
+import com.eomcs.lms.dao.mariadb.MemberDaoImpl;
+import com.eomcs.lms.dao.mariadb.PhotoBoardDaoImpl;
+import com.eomcs.lms.dao.mariadb.PhotoFileDaoImpl;
+import com.eomcs.sql.DataSource;
+import com.eomcs.sql.PlatformTransactionManager;
+
+// 애플리케이션이 시작되거나 종료될 때
+// 데이터를 로딩하고 저장하는 일을 한다.
+//
+public class DataLoaderListener implements ApplicationContextListener {
+
+  String jdbcUrl = "jdbc:mariadb://localhost:3306/studydb";
+  String username = "study";
+  String password = "1111";
+
+  @Override
+  public void contextInitialized(Map<String, Object> context) {
+    try {
+      System.out.println("데이터를 로딩합니다.");
+
+      // 커넥션 팩토리 준비
+      DataSource dataSource = new DataSource(jdbcUrl, username, password);
+      context.put("dataSource", dataSource);
+
+      // mybatis 준비
+      InputStream inputStream = Resources.getResourceAsStream(//
+          "com/eomcs/lms/conf/mybatis-config.xml");
+      SqlSessionFactory sqlSessionFactory = //
+          new SqlSessionFactoryBuilder().build(inputStream);
+
+      context.put("boardDao", new BoardDaoImpl(sqlSessionFactory));
+      context.put("memberDao", new MemberDaoImpl(sqlSessionFactory));
+      context.put("lessonDao", new LessonDaoImpl(sqlSessionFactory));
+      context.put("photoBoardDao", new PhotoBoardDaoImpl(sqlSessionFactory));
+      context.put("photoFileDao", new PhotoFileDaoImpl(sqlSessionFactory));
+
+      // 트랜잭션 관리자 준비
+      PlatformTransactionManager txManager = new PlatformTransactionManager(dataSource);
+      context.put("transactionManager", txManager);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }// contextInitialized
+
+  @Override
+  public void contextDestroyed(Map<String, Object> context) {
+    System.out.println("데이터를 저장합니다.");
+    // 애플리케이션이 종료될 때,
+    // 모든 DB 커넥션을 명시적으로 끊어준다.
+    // 그러면 DBMS는 timeout 될 때까지 기다릴 필요 없이
+    // 클라이언트와 연결된 스레드를 즉시 해제시킬 수 있다.
+    DataSource dataSource = (DataSource) context.get("dataSource");
+    dataSource.clean();
+  }// contextDestroyed
+
+}
---
+++ bitcamp-project-server/version/v43_1/src/main/java/com/eomcs/lms/servlet/PhotoBoardUpdateServlet.java
@@ -0,0 +1,92 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import com.eomcs.lms.dao.PhotoBoardDao;
+import com.eomcs.lms.dao.PhotoFileDao;
+import com.eomcs.lms.domain.PhotoBoard;
+import com.eomcs.lms.domain.PhotoFile;
+import com.eomcs.sql.PlatformTransactionManager;
+import com.eomcs.sql.TransactionTemplate;
+import com.eomcs.util.Prompt;
+
+public class PhotoBoardUpdateServlet implements Servlet {
+  PhotoBoardDao photoBoardDao;
+  PhotoFileDao photoFileDao;
+  TransactionTemplate transactionTemplate;
+
+  public PhotoBoardUpdateServlet(PhotoBoardDao photoBoardDao, PhotoFileDao photoFileDao,
+      PlatformTransactionManager txManager) {
+    this.photoBoardDao = photoBoardDao;
+    this.photoFileDao = photoFileDao;
+    this.transactionTemplate = new TransactionTemplate(txManager);
+  }
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+    int no = Prompt.getInt(in, out, "번호?");
+    PhotoBoard old = photoBoardDao.findByNo(no);
+
+    if (old == null) {
+      out.println("해당 번호의 사진 게시글이 없습니다.");
+      return;
+    }
+    PhotoBoard photoBoard = new PhotoBoard();
+    photoBoard.setTitle(
+        Prompt.getString(in, out, String.format("제목(%s)?", old.getTitle()), old.getTitle()));
+    photoBoard.setNo(no);
+
+    transactionTemplate.execute(() -> {
+      if (photoBoardDao.update(photoBoard) == 0) {
+        throw new Exception("사진 게시글 변경에 실패했습니다.");
+      }
+      printPhotoFiles(out, no);
+      out.println("\n사진은 일부만 변경할 수 없습니다.");
+      String response = Prompt.getString(in, out, "사진을 변경하시겠습니다? (Y/N)");
+      if (response.equalsIgnoreCase("y")) {
+        // 이 사진 게시글에 첨부된 기존 파일을 모두 삭제한다.
+        photoFileDao.deleteAll(no);
+
+        List<PhotoFile> photoFiles = inputPhotoFiles(in, out);
+        // ArryaList에 들어있는 PhotoFile 데이터를 lms_photo_file 테이블에 저장한다.
+        for (PhotoFile photoFile : photoFiles) {
+          photoFile.setBoardNo(no);
+          photoFileDao.insert(photoFile);
+        }
+      }
+      out.println("사진 게시글을 변경했습니다.");
+      return null;
+    });
+  }
+
+  private List<PhotoFile> inputPhotoFiles(Scanner in, PrintStream out) {
+    // 새로 등록할 첨부파일을 입력받는다.
+    out.println("최소 한 개의 사진 파일을 등록해야 합니다.");
+    out.println("파일명 입력 없이 엔터를 치면 파일 추가를 마칩니다.");
+    ArrayList<PhotoFile> files = new ArrayList<>();
+    while (true) {
+      String filepath = Prompt.getString(in, out, "사진 파일?");
+      if (filepath.length() == 0) {
+        if (files.size() > 0) {
+          break;
+        } else {
+          out.println("최소 한 개의 사진 파일을 등록해야합니다.");
+          continue;
+        }
+      }
+      files.add(new PhotoFile().setFilePath(filepath));
+    }
+    return files;
+  }
+
+  private void printPhotoFiles(PrintStream out, int boardNo) throws Exception {
+    out.println("사진파일: ");
+    List<PhotoFile> photoFiles = photoFileDao.findAll(boardNo);
+    for (PhotoFile file : photoFiles) {
+      out.printf("> %s\n", file.getFilePath());
+    }
+  }
+
+}
