e12f84b7bf05f6801b850f341be874d84f07237b
kim-hyunjin <hyunjin1612@gmail.com>
2020-03-23 17:16:24 +0900

    spring ioc 학습

--- bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex10/a/Exam01.java
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex10/a/Exam01.java
@@ -1,5 +1,5 @@
 // 클래스를 이용하여 스프링 설정하기
-package com.eomcs.spring.ioc.ex11;
+package com.eomcs.spring.ioc.ex10.a;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,7 +11,6 @@ public class Exam01 {
     ApplicationContext iocContainer = new AnnotationConfigApplicationContext(AppConfig.class);
 
     SpringUtils.printBeanList(iocContainer);
-
   }
 
 }
