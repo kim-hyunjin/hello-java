b746da3d9efc6252fe61436df703548b9e12710c
kim-hyunjin <hyunjin1612@gmail.com>
2020-04-07 16:20:10 +0900

    refresh, redirect 학습

--- bitcamp-java-web/src/main/java/com/eomcs/web/ex08/Servlet100.java
+++ bitcamp-java-web/src/main/java/com/eomcs/web/ex08/Servlet100.java
@@ -1,5 +1,5 @@
 // 리프래시 또는 리다이렉트 최종 요청 페이지
-package bitcamp.ex08;
+package com.eomcs.web.ex08;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,20 +12,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ex08/s100")
 @SuppressWarnings("serial")
 public class Servlet100 extends HttpServlet {
-  
+
   @Override
   protected void service(
-      HttpServletRequest request, 
+      HttpServletRequest request,
       HttpServletResponse response)
-      throws ServletException, IOException {
-    
+          throws ServletException, IOException {
+
     // 이 서블릿은 직접 실행할 용도로 만든 것이 아니다.
     // 리프래시와 리다이렉트를 연습할 용도로 만들었다.
-    // 
+    //
     response.setContentType("text/plain;charset=UTF-8");
     PrintWriter out = response.getWriter();
-    
+
     out.println("반가워요 - /ex08/s100");
-   }
+  }
 }
 
