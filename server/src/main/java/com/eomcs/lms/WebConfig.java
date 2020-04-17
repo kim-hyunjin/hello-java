0f9b13465a9d97e604ff5b32bd85aa18567f1efb
Kimhyunjin <53554345+hjkim0317@users.noreply.github.com>
2020-04-17 23:23:27 +0900

    string to date 변환문제 해결

---
+++ server/src/main/java/com/eomcs/lms/WebConfig.java
@@ -0,0 +1,20 @@
+package com.eomcs.lms;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+      DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+      registrar.setUseIsoFormat(true);
+      registrar.registerFormatters(registry);
+  }
+
+}
